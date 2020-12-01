1ee8c59c19df7b19358c12ec6ac8c77ef87ea543
Scottie <avscott@pacbell.net>
2020-12-01 02:19:29 -0800

    add initialize data to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { Component, Fragment } from 'react'
+
+// bring in router functions
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+// bring in connect so all of the child components can access the state
+import { connect } from 'react-redux'
+
+// bring in the shared action that initiates obtaining the initial data
+import { handleInitialdata } from './actions/shared'
+
+// bring in components
 import Nav from './components/Nav'
 import LogIn from './components/LogIn'
 import Dashboard from './components/Dashboard'
 import QuestionPage from './components/QuestionPage'
 import NewQuestion from './components/NewQuestion'
 import LeaderBoard from './components/LeaderBoard'
+
+// bring in css file
 import './css/app.css'
 
+
+
 class App extends Component {
+  componentDidMount() {
+    // must put connect in the export default to access dispatch in props
+    this.props.dispatch(handleInitialdata())
+  }
+
   render() {
     return (
       <Router>
@@ -28,4 +47,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
