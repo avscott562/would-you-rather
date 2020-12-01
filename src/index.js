1ddba4caab0dd14a8e2b2c3d2084a5686d81079d
Scottie <avscott@pacbell.net>
2020-12-01 01:49:46 -0800

    add store

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import './index.css';
 import App from './App';
 import 'materialize-css/dist/css/materialize.min.css'
 
+// bring in createStore from redux
+import { createStore } from 'redux' 
+
+// bring in provider from react-redux
+import { Provider } from 'react-redux'
+
+// bring in root reducer - by mapping to reducers folder, it will automatically use the index.js file
+import reducer from './reducers'
+
+// create store - pass reducer to createStore so the store knows what state it should have
+// pass middleware as the second argument
+const store = createStore(reducer)
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
